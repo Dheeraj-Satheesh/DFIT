98b376146fe32cdc181c731cbd1f5e6506841474
Dheeraj-Satheesh <satheeshdheeraj2004@gmail.com>
2026-02-01 17:09:42 +0530

    85. updated dpmr trainings

--- convert_dpmr_trainings.py
+++ convert_dpmr_trainings.py
@@ -36,7 +36,7 @@ STATE_NAME_MAP = {
     "tot": "All States"
 }
 
-YEARS = ["2020", "2021", "2022", "2023", "2024"]
+YEARS = ["2020", "2021", "2022", "2023", "2024","2025"]
 
 # FOLDER where your raw DPMR jsons are
 INPUT_FOLDER = "DPMR Trainings"
